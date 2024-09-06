d981bec3d93733c813d868ca0d6dc7f7248cf720
yy2hi <ypy2.hi@gmail.com>
2024-09-06 14:11:33 +0900

    12

--- admin/db_sql_admin.py
+++ admin/db_sql_admin.py
@@ -28,13 +28,41 @@ def result_sql_admin():
                 global result_ADMIN_ID
                 result_ADMIN_ID = int(result_sql_ADMIN_ID[0][0])
 
+                # host os group id
+                sql_OS_GROUP_ID = "SELECT OS_GROUP_ID FROM HOST_OS_GROUP WHERE OS_GROUP_NAME='test';"
+                result_sql_OS_GROUP_ID = curs.execute(sql_OS_GROUP_ID).fetchall()
+                global result_OS_GROUP_ID
+                result_OS_GROUP_ID = int(result_sql_OS_GROUP_ID[0][0])
 
-                # 사용할 host os id 추출 (compute17)
-                sql_HOST_OS_ID = "SELECT OS_ID FROM HOST_OS WHERE HOSTNAME='compute17';"
-                result_sql_HOST_OS_ID = curs.execute(sql_HOST_OS_ID).fetchall()
-                global result_HOST_OS_ID
-                result_HOST_OS_ID = int(result_sql_HOST_OS_ID[0][0])
+                # HOST OS GROUP MEMBER 조인 테스트용 ID (server_101)
+                sql_HOM_HOST_OS_ID = "SELECT OS_ID FROM HOST_OS WHERE HOSTNAME='server_101';"
+                result_sql_HOM_HOST_OS_ID = curs.execute(sql_HOM_HOST_OS_ID).fetchall()
+                global result_HOM_HOST_OS_ID
+                result_HOM_HOST_OS_ID = int(result_sql_HOM_HOST_OS_ID[0][0])
 
+                # 컴퓨트 노드 HOST_OS_ID (compute17)
+                sql_COM_HOST_OS_ID = "SELECT OS_ID FROM HOST_OS WHERE HOSTNAME='compute17';"
+                result_sql_COM_HOST_OS_ID = curs.execute(sql_COM_HOST_OS_ID).fetchall()
+                global result_COM_HOST_OS_ID
+                result_COM_HOST_OS_ID = int(result_sql_COM_HOST_OS_ID[0][0])
+
+                # 네트워크 노드 HOST_OS_ID (master9)
+                sql_NET_HOST_OS_ID = "SELECT OS_ID FROM HOST_OS WHERE HOSTNAME='master9';"
+                result_sql_NET_HOST_OS_ID = curs.execute(sql_NET_HOST_OS_ID).fetchall()
+                global result_NET_HOST_OS_ID
+                result_NET_HOST_OS_ID = int(result_sql_NET_HOST_OS_ID[0][0])
+
+                # BlkDevDomain ID
+                sql_BlkDevDomain = "select ID from STORAGE_BLOCK_DEVICE_DOMAIN;"
+                result_sql_BlkDevDomain = curs.execute(sql_BlkDevDomain).fetchall()
+                global result_BlkDevDomain
+                result_BlkDevDomain = int(result_sql_BlkDevDomain[0][0])
+
+                # FSDomain_ID
+                sql_FSDomain_ID = "select ID from STORAGE_FILESYSTEM_DOMAIN;"
+                result_sql_FSDomain_ID = curs.execute(sql_FSDomain_ID).fetchall()
+                global result_FSDomain_ID
+                result_FSDomain_ID = int(result_sql_FSDomain_ID[0][0])
 
                 # 연결 닫기
                 conn.close()
--- admin/make_admin_json.py
+++ admin/make_admin_json.py
@@ -42,6 +42,38 @@ def admin():
     db_sql_admin.result_sql_admin()
 
 
+    # HOST_OS_GROUP 생성
+    CreateHostOSGroup = {
+        "header": {
+            "targetServiceName": "host-os-service/com.tmax.tmaxcloud.hom.compute.os.CreateHostOSGroup",
+            "messageType": "REQUEST"
+        },
+        "body": {
+            "GROUP_NAME": "test",
+            "TYPE": "test",
+            "DESCRIPTION": "test"
+        }
+    }
+    file_path, total_num_files,base_direc= create_json_file("CreateHostOSGroup.json", CreateHostOSGroup, total_num_files)
+    success_num_files, fail_num_files = sapgo_result.admin_sapgo_result(base_direc,file_path, success_num_files, fail_num_files)
+    db_sql_admin.rseult_sql_admin()
+
+
+    # HOST OS GROUP MEMBER 생성
+    CreateHostOSGroupMember= {
+        "header": {
+            "targetServiceName": "host-os-service/com.tmax.tmaxcloud.hom.compute.dx_service.CreateHostOSGroupMember",
+            "messageType": "REQUEST"
+        },
+        "body": {
+            "os_group_id": db_sql_admin.result_OS_GROUP_ID,
+            "host_os_id": db_sql_admin.result_HOM_HOST_OS_ID
+        }
+    }
+    file_path, total_num_files,base_direc = create_json_file("CreateHostOSGroupMember.json", CreateHostOSGroupMember, total_num_files)
+    success_num_files, fail_num_files = sapgo_result.admin_sapgo_result(base_direc,file_path, success_num_files, fail_num_files)
+    db_sql_admin.rseult_sql_admin()
+
     # # master8
     # CreateHostInterface = {
     #     "header": {
@@ -106,6 +138,7 @@ def admin():
     # success_num_files, fail_num_files = sapgo_result.admin_sapgo_result(base_direc,file_path, success_num_files, fail_num_files)
     # db_sql_admin.result_sql_admin()
 
+
     # compute17 용도 지정 (bvt)
     CreateHostInterfaceUsage  = {
         "header": {
@@ -115,14 +148,37 @@ def admin():
             "contentType": "TEXT"
         },
         "body": {
-                    "osId":db_sql_admin.result_HOST_OS_ID,
-                    "interfaceName":"enp2s0",
-                    "ifUsage":["control","ceph_front","internal"]
+                "osId":db_sql_admin.result_HOST_OS_ID,
+                "interfaceName":"enp2s0",
+                "ifUsage":["control","ceph_front","internal"]
         }
     }
+
     file_path, total_num_files, base_direc= create_json_file("CreateHostInterfaceUsage.json", CreateHostInterfaceUsage, total_num_files)
     success_num_files, fail_num_files = sapgo_result.admin_sapgo_result(base_direc,file_path, success_num_files, fail_num_files)
     db_sql_admin.result_sql_admin()
+
+
+    # compute17 CreateComputeHostService 생성 (bvt)
+    CreateComputeHostService = {
+       "header": {
+            "targetServiceName": "inframaster-compute/com.tmax.tmaxcloud.inframaster.compute.master.service.compute.host.CreateComputeHostService",
+            "messageType": "REQUEST"
+        },
+        "body": {
+            "host_os_id" : db_sql_admin.result_COM_HOST_OS_ID,
+            "host_os_group_id" : 2,
+            "memory_overcommit_ratio" : "2.0",
+            "memory_limit_ratio" : "1",
+            "interface_usage" : [{"name" : "enp2s0" , "usage" : "internal"}, {"name" : "enp2s0" , "usage" : "control"},
+            {"name" : "enp2s0", "usage" : "ceph-front", "ip" : "172.21.5.17", "dns" : "8.8.8.8", "gateway" : "172.21.5.1", "netmask" : 24}]
+        }
+    }
+
+    file_path, total_num_files, base_direc= create_json_file("CreateComputeHostService.json", CreateComputeHostService, total_num_files)
+    success_num_files, fail_num_files = sapgo_result.admin_sapgo_result(base_direc,file_path, success_num_files, fail_num_files)
+    db_sql_admin.result_sql_admin()
+
 
     # master9 네트워크 노드 등록 (bvt)
     SettingNetworkHostService  = {
@@ -131,7 +187,7 @@ def admin():
             "messageType": "REQUEST"
         },
         "body": {
-            "osId":2,
+            "osId":db_sql_admin.result_NET_HOST_OS_ID,
             "interfaceSetting": [                
                     {"interfaceName":  "eno1", "usage": "control", "ip" : "172.21.5.9", "netmask": "255.255.255.0", "gateway": None, "dns":"8.8.8.8"},
                     {"interfaceName":  "eno1", "usage": "internal", "ip" : "175.21.5.9", "netmask": "255.255.255.0", "gateway": None, "dns":None},
@@ -145,6 +201,7 @@ def admin():
     success_num_files, fail_num_files = sapgo_result.admin_sapgo_result(base_direc,file_path, success_num_files, fail_num_files)
     db_sql_admin.result_sql_admin()
  
+
     # IP POOL 등록 (bvt)
     CreateClusterIppoolService  = {
         "header": {
@@ -164,6 +221,76 @@ def admin():
         }
     }
     file_path, total_num_files, base_direc= create_json_file("CreateClusterIppoolService.json", CreateClusterIppoolService, total_num_files)
+    success_num_files, fail_num_files = sapgo_result.admin_sapgo_result(base_direc,file_path, success_num_files, fail_num_files)
+    db_sql_admin.result_sql_admin()
+
+
+    # storage 대시보드 확인
+    DashboardStorageClusterInfo = {
+    "header": {
+        "targetServiceName": "storage/com.tmax.tmaxcloud.storage.master.DashboardStorageClusterInfo",
+        "requestId": 1,
+        "messageType": "REQUEST",
+        "contentType": "TEXT"
+        },
+        "body": {
+        }
+    }
+
+    file_path, total_num_files, base_direc= create_json_file("DashboardStorageClusterInfo.json", DashboardStorageClusterInfo, total_num_files)
+    success_num_files, fail_num_files = sapgo_result.admin_sapgo_result(base_direc,file_path, success_num_files, fail_num_files)
+    db_sql_admin.result_sql_admin()
+
+
+    # OsTypeList 확인
+    OsTypeList = {
+        "header": {
+            "targetServiceName": "storage/com.tmax.tmaxcloud.storage.master.OsTypeList",
+            "requestId": 1,
+            "messageType": "REQUEST",
+            "contentType": "TEXT"
+        },
+        "body": {
+        }
+    }
+
+    file_path, total_num_files, base_direc= create_json_file("OsTypeList.json", OsTypeList, total_num_files)
+    success_num_files, fail_num_files = sapgo_result.admin_sapgo_result(base_direc,file_path, success_num_files, fail_num_files)
+    db_sql_admin.result_sql_admin()
+
+
+    # BlkDevDomainGet 확인
+    BlkDevDomainGet = {
+    "header": {
+        "targetServiceName": "storage/com.tmax.tmaxcloud.storage.master.BlkDevDomainGet",
+        "requestId": 1,
+        "messageType": "REQUEST",
+        "contentType": "TEXT"
+    },
+    "body": {
+        "id": db_sql_admin.result_BlkDevDomain
+    }
+    }
+
+    file_path, total_num_files, base_direc= create_json_file("BlkDevDomainGet.json", BlkDevDomainGet, total_num_files)
+    success_num_files, fail_num_files = sapgo_result.admin_sapgo_result(base_direc,file_path, success_num_files, fail_num_files)
+    db_sql_admin.result_sql_admin()
+
+
+    # FSDomain 확인
+    FsDomainGet = {
+        "header": {
+            "targetServiceName": "storage/com.tmax.tmaxcloud.storage.master.FsDomainGet",
+            "requestId": 1,
+            "messageType": "REQUEST",
+            "contentType": "TEXT"
+        },
+        "body": {
+            "id": db_sql_admin.result_FSDomain_ID
+        }
+    }
+
+    file_path, total_num_files, base_direc= create_json_file("FsDomainGet.json", FsDomainGet, total_num_files)
     success_num_files, fail_num_files = sapgo_result.admin_sapgo_result(base_direc,file_path, success_num_files, fail_num_files)
     db_sql_admin.result_sql_admin()
 
